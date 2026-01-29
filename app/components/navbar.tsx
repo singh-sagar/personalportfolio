64da101962d7f4c6fb6caed8dacad9043f3ea20c
Sagar Singh <sagarsingh@Sagars-MacBook-Air.local>
2026-01-28 20:23:32 -0600

    added the bio with tldr;

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,9 +1,14 @@
+import VerifiedIcon from '../components/verifiedIcon';
 export default function Navbar(){
     return(
       <nav className="flex justify-center items-center bg-black h-[100px] w-screen">
         {/* justify-between makes the first child on left and rest on right*/ }
         <div className="h-full max-w-6xl w-full bg-red-400 flex justify-between items-center px-20">
-            <div className="font-bold text-black text-4xl font-stretch-extra-expanded">Sagar Singh</div>
+            <div className="flex items-center">
+                <a className="font-bold text-black text-4xl font-stretch-extra-expanded">Sagar Singh</a>
+                <VerifiedIcon className="w-10 h-10 ml-2 text-blue-500" />
+            </div>
+          
             {/* gap gives equal gaps between child nodes*/ }
             <ul className="flex gap-6"  >
                 <a className="text-white text-xl bg-black rounded-lg px-2" href="/">Home</a>
--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,27 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-white font-sans ">
-      <div className=" min-h-screen max-w-6xl w-full bg-red-100">
-       
+    <div className="flex flex-col min-h-screen bg-white font-sans ">
+      
+   
+      <div className=" min-h-screen max-w-6xl w-full bg-red-100 pt-20 mx-auto">
+           {/* First Div: About Section */}
+        {/* need fixing */}
+       <div className=" max-w-6xl w-full  p-1  flex  justify-between items-center px-20 bg-black"> 
+          <div className="bg-red-100 text-black ">
+              <p className="text-xl font-extrabold ">about</p>
+              <div className="text-lg mt-3 flex flex-col lg:gap-y-2">
+                <p className="font-bold">tldr; Data scientist and analyst, bridging the gap between business strategy and code.</p>
+                <p>drawn to automation, cloud engineering, and building systems that scale.</p>
+                <p>curious about machine learning, predictive analytics, and solving complex problems.</p>
+              </div>
+          </div>
+        </div>
       </div>
+
+
      
-    </div>
+     
+    // </div>
   );
 }

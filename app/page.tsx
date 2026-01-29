17d8f8b9bb1a09c20b141725cde4ccea6b6918a3
Sagar Singh <sagarsingh@Sagars-MacBook-Air.local>
2026-01-28 20:46:26 -0600

    added edcuation div

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ export default function Home() {
     <div className="flex flex-col min-h-screen bg-white font-sans ">
       
    
-      <div className=" min-h-screen max-w-6xl w-full bg-red-100 pt-20 mx-auto">
+      <div className=" min-h-screen max-w-6xl w-full bg-red-100 pt-20 mx-auto flex flex-col gap-20">
            {/* First Div: About Section */}
         {/* need fixing */}
-       <div className=" max-w-6xl w-full  p-1  flex  justify-between items-center px-20 bg-black"> 
+       <div className=" max-w-6xl w-full  flex  justify-between items-center px-20 bg-black"> 
           <div className="bg-red-100 text-black ">
               <p className="text-xl font-extrabold ">about</p>
               <div className="text-lg mt-3 flex flex-col lg:gap-y-2">
@@ -17,7 +17,16 @@ export default function Home() {
                 <p>curious about machine learning, predictive analytics, and solving complex problems.</p>
               </div>
           </div>
-        </div>
+       </div>
+       {/* second div : holding the education sections*/}
+       <div className="max-w-6xl w-full flex justify-between items-center px-20 bg-black">
+         <div className="bg-red-100 max-w-6xl w-full p-20 ">
+           <div className="">
+
+           </div>
+            
+         </div>
+       </div>
       </div>
 
 
